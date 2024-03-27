0e554583edd1428086bf928cef4800454c8fa188
Işınsu Karagöz <118214808+isinsuu@users.noreply.github.com>
2024-03-27 11:31:23 +0300

    auto configuration

--- RazorPagesMovie/obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
+++ RazorPagesMovie/obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb0e530a46c9cb527d046e725edbacc028149821")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+443f4d1e12829de0c3b1d14e642fba5fb7c40b32")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
