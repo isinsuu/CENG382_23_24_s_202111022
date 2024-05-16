16e7fdfdd0a39db00d4d9cd6ab0e3dcfd9f90dc3
Işınsu Karagöz <118214808+isinsuu@users.noreply.github.com>
2024-05-16 21:11:40 +0300

    I created the CreateRooms and DisplayRooms pages accordingly, implemented basic CRUD operations such as adding and listing the rooms and added some css.

---
+++ NewProject/Pages/CreateRooms.cshtml.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class CreateRoomsModel : PageModel
+    {
+        private readonly AppDbContext _context;
+
+        public CreateRoomsModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Room NewRoom { get; set; } = new Room(); 
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid || NewRoom == null)
+            {
+                return Page();
+            }
+
+            _context.Rooms.Add(NewRoom);
+            _context.SaveChanges();
+            return RedirectToPage("/DisplayRooms");
+        }
+    }
+}
