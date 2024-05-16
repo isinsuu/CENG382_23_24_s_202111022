36694ecb65dee52b066f39c6f26a4427371df768
Işınsu Karagöz <118214808+isinsuu@users.noreply.github.com>
2024-05-16 15:38:41 +0300

    I successfully managed the migrations and updated my database schema by adding Reservation Table.

---
+++ NewProject/Models/Reservation.cs
@@ -0,0 +1,7 @@
+public class Reservation
+{
+    public int Id { get; set; } 
+    public Room? Room { get; set; } 
+    public DateTime DateTime { get; set; } 
+    public string? ReservedBy { get; set; } 
+}
